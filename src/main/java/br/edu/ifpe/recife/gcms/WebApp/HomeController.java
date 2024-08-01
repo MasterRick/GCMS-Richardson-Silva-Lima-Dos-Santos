8824cd235aacd44f4481e3541313f299a68054f7
Richardson Silva <richardsonsilva97@hotmail.com>
2024-08-01 19:37:31 -0300

    Adicionado a rota hotfix na branch hotfix
    Adicionado a rota hotfix na branch hotfix

--- src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -1,4 +1,5 @@
 package br.edu.ifpe.recife.gcms.WebApp;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,11 @@ public class HomeController {
     @RequestMapping("/richardson")
     public @ResponseBody String greetingFulano() {
         return "Hello, Richardson Silva Lima dos Santos!";
+    }
+
+    @RequestMapping("/hotfix")
+    public @ResponseBody String hotfix() {
+        return "hotfix";
     }
 
 }
--- src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -1,4 +1,5 @@
 package br.edu.ifpe.recife.gcms.WebApp;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.hamcrest.Matchers.containsString;
@@ -10,27 +11,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class WebAppApplicationTests {
 
-@Autowired
-private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-@Test
-public void shouldReturnDefaultMessage() throws Exception {
-	this.mockMvc.perform(get("/"))
-		.andDo(print()).andExpect(status().isOk())
-		.andExpect(content()
-		.string(containsString("Hello, World")));
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(content()
+						.string(containsString("Hello, World")));
 	}
 
-@Test
-public void shouldReturnName() throws Exception {
-	this.mockMvc.perform(get("/richardson"))
-		.andDo(print()).andExpect(status().isOk())
-		.andExpect(content()
-		.string(containsString("Hello, Richardson Silva Lima dos Santos!")));
+	@Test
+	public void shouldReturnName() throws Exception {
+		this.mockMvc.perform(get("/richardson"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(content()
+						.string(containsString("Hello, Richardson Silva Lima dos Santos!")));
+	}
+
+	@Test
+	public void shouldReturnHotfix() throws Exception {
+		this.mockMvc.perform(get("/hotfix"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(content()
+						.string(containsString("hotfix")));
 	}
 }
